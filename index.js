ff4b79ebadf40175604ad145445b5f8e8940247b
Brian Seabolt <brianmseabolt@gmail.com>
2018-09-20 09:49:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NDkyMDA5NzE4NjA1MjgzMzM5.DoQMng.GCCOIYfG73D3Zwj4A6yV2I9CeEE');
