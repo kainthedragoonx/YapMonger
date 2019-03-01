53c1eb5edaa3ebea50f128962a7d0e0c343fe814
Brian Seabolt <brianmseabolt@gmail.com>
2019-03-01 18:03:06 -0500

    updates to get dynamic messaging working better

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,7 @@
 module.exports = {
-	name: 'ping', description: 'Ping!',
+	name: 'ping', 
+	description: 'Ping!',
 	execute(message, args) {
-		if (!args.length) {
-			return message.channel.send(`Hey ${message.author}, RTFM, dipshit!`);
-		}
-		message.channel.send('Fuck off!');
+		message.channel.send(`Hey ${message.author}, RTFM, dipshit!`);
 	},
 };
